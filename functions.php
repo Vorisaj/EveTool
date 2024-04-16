658bdc620dc26cc34cc0aaf21050fc3ca07b58bc
Vorisaj <159558719+Vorisaj@users.noreply.github.com>
2024-04-16 11:57:38 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+
+function DownloadAllAPIPages($url, $char_id) {
+  $page = 1;
+  $trials = 0;
+  $pages = array();
+  $opts = null;
+  if ($char_id != null) $opts = GetOptsForCharacterId($char_id);
+
+  while(true) {
+    
+    $furl = $url."page=".$page;
+    $json = http_request($furl, $opts);
+    $json = json_decode($json); 
+    if(isset($json->error)) {
+        if ($json->error == "token is expired") {
+          RefreshTokens();
+          $opts = GetOptsForCharacterId($char_id);
+          $trials = $trials + 1;
+          if ($trials > 3) {
+            echo "[DownloadAllAPIPages] Token refresh error!";
+            return FALSE;
+          }
+          continue;
+        }
+        if (strpos($json->error, "Undefined 404 response") === false && $json->error != "Requested page does not exist!") {
+            echo "[DownloadAllAPIPages] ".$furl." Error: ".$json->error."\n";
+            return FALSE;
+        }
+        break;
+    }
+    if($json == null) {
+        continue;
+    }
+    for($i = 0; $i < count($json); $i++) {
+        array_push($pages, $json[$i]);
+    }
+    $page = $page + 1;
+  }
+  return $pages;
+}
+
+?>
